67e5818e6ff52c49d08be76550be798a3773c040
MrBT007 <bhuttushar2019@gmail.com>
2022-01-08 18:25:19 +0530

    done upto getting orders in My Orders

--- app/src/main/java/com/example/mharaj/DetailActivity.kt
+++ app/src/main/java/com/example/mharaj/DetailActivity.kt
@@ -22,58 +22,58 @@ class DetailActivity : AppCompatActivity()
         var helper = DBHelper(this)
 
 
-            var foodimageview = findViewById<ImageView>(R.id.foodDetailImg)
-            var fooddetailprice = findViewById<TextView>(R.id.detailfoodprice)
-            var foodname = findViewById<TextView>(R.id.foodDetailName)
-            var fooddetaildesc = findViewById<TextView>(R.id.detailDescreption)
-            var quantity = findViewById<TextView>(R.id.quantity)
-            var add = findViewById<ImageView>(R.id.add)
-            var subtract = findViewById<ImageView>(R.id.subtract)
+        var foodimageview = findViewById<ImageView>(R.id.foodDetailImg)
+        var fooddetailprice = findViewById<TextView>(R.id.detailfoodprice)
+        var foodname = findViewById<TextView>(R.id.foodDetailName)
+        var fooddetaildesc = findViewById<TextView>(R.id.detailDescreption)
+        var quantity = findViewById<TextView>(R.id.quantity)
+        var add = findViewById<ImageView>(R.id.add)
+        var subtract = findViewById<ImageView>(R.id.subtract)
 
-            foodimageview.setImageResource(image)
-            fooddetailprice.setText(price.toString())
-            foodname.setText(name)
-            fooddetaildesc.setText(description)
-            var qnt = quantity.text.toString()
+        foodimageview.setImageResource(image)
+        fooddetailprice.setText(price.toString())
+        foodname.setText(name)
+        fooddetaildesc.setText(description)
+        var qnt = quantity.text.toString()
 
-            val orderbutton = findViewById<Button>(R.id.orderButton)
+        val orderbutton = findViewById<Button>(R.id.orderButton)
 
-            val personName = findViewById<TextView>(R.id.editTextTextPersonName)
-            val personPhone = findViewById<TextView>(R.id.editTextPhone)
+        val personName = findViewById<TextView>(R.id.editTextTextPersonName)
+        val personPhone = findViewById<TextView>(R.id.editTextPhone)
 
-            var a = qnt.toInt()
-            add.setOnClickListener()
-            {
-                a++
+        var a = qnt.toInt()
+        add.setOnClickListener()
+        {
+            a++
+            qnt = (a).toString()
+            quantity.text = qnt
+        }
+        subtract.setOnClickListener()
+        {
+            if (a >= 2) {
+                a = a - 1
                 qnt = (a).toString()
                 quantity.text = qnt
             }
-            subtract.setOnClickListener()
-            {
-                if (a >= 2) {
-                    a = a - 1
-                    qnt = (a).toString()
-                    quantity.text = qnt
-                }
-            }
-
-            orderbutton.setOnClickListener()
-            {
-                val isInserted: Boolean = helper.insertOrder(
-                    personName.text.toString(),
-                    personPhone.text.toString(),
-                    price,
-                    image,
-                    description.toString(),
-                    name.toString(),
-                    qnt.toInt()
-                )
-                if (isInserted) {
-                    Toast.makeText(this, "Data Success", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-                }
-            }
         }
 
+        orderbutton.setOnClickListener()
+        {
+            val isInserted: Boolean = helper.insertOrder(
+                personName.text.toString(),
+                personPhone.text.toString(),
+                price,
+                image,
+                description.toString(),
+                name.toString(),
+                qnt.toInt()
+            )
+            if (isInserted) {
+                Toast.makeText(this, "Data Success", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+}
